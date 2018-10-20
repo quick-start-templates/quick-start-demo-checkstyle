70a9ab051e3e470eb693de4399310e26116f0e65
kezhenxu94 <kezhenxu94@163.com>
2018-10-20 23:58:25 +0800

    Initial Commit

---
+++ src/main/java/me/kezhenxu94/demo/App.java
@@ -0,0 +1,13 @@
+package me.kezhenxu94.demo;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
